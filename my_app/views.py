fd8d71fc1addc4662410369dee0c8e6397ab5848
kewablay <kojokewajunior123@gmail.com>
2021-08-20 02:09:08 +0000

    added images to the new_search views page

--- my_app/views.py
+++ my_app/views.py
@@ -20,21 +20,28 @@ def new_search(request):
     # ------- ITEMS TO SCRAPE ------------
     items_list = soup.find_all(class_ = 'list-no-v2-inner m-gallery-product-item-v2 img-switcher-parent')
     item_name = soup.find(class_ = 'elements-title-normal__outter').get_text()
-    item_price = soup.find(class_ = 'elements-offer-price-normal__price').get_text()
+    item_price = soup.find(class_ = 'organic-gallery-offer-section__price').find('span').get_text()
     item_review = soup.find(class_ = 'seb-supplier-review text-ellipsis list-no-v2-decisionsup__element')
     item_url = soup.find(class_ = 'elements-title-normal one-line').get('href')
+    item_image = soup.find(class_ = 'seb-img-switcher__imgs').get('data-image')
+    print(item_price)
+    # for items in items_list:
+    #     print(items.find(class_ = 'seb-img-switcher__imgs').get('data-image'))
+
     # print(item_url)
     
+
 
     final_item_list = []
 
     for items in items_list:
         item_name = items.find(class_ = 'elements-title-normal__outter').get_text()
-        item_price = items.find(class_ = 'elements-offer-price-normal__price').get_text()
+        item_price = items.find(class_ = 'organic-gallery-offer-section__price').find('span').get_text()    
         item_url = items.find(class_ = 'elements-title-normal one-line').get('href')
-        # item_review = items.find(class_ = 'seb-supplier-review text-ellipsis list-no-v2-decisionsup__element').get_text()
+        item_image = items.find(class_ = 'seb-img-switcher__imgs').get('data-image')
 
-        final_item_list.append((item_name, item_price, item_url, item_review))
+
+        final_item_list.append((item_name, item_price, item_url, item_image))
 
 
     context = {
